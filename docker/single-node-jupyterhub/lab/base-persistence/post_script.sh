3689fc6e744bf01cf81aaa7ed653f8e4ff8d006f
Marica Antonacci <marica.antonacci@ba.infn.it>
2024-04-22 20:36:25 +0200

    Fix s3 endpoint

--- docker/single-node-jupyterhub/lab/base-persistence/post_script.sh
+++ docker/single-node-jupyterhub/lab/base-persistence/post_script.sh
@@ -10,13 +10,13 @@ mkdir -p /s3/"${USERNAME}"
 mkdir -p /s3/scratch
 
 sleep 1s && nice -n 19 sts-wire https://iam.cloud.infn.it/ \
-    "${USERNAME}" https://minio.cloud.infn.it/ \
+    "${USERNAME}" https://rgw.cloud.infn.it/ \
     "/${USERNAME}" "/s3/${USERNAME}" \
     --localCache full --tryRemount --noDummyFileCheck \
     --localCacheDir "${BASE_CACHE_DIR}/${USERNAME}" \
     &>"/var/log/sts-wire/mount_log_${USERNAME}.txt" &
 sleep 2s && nice -n 19 sts-wire https://iam.cloud.infn.it/ \
-    scratch https://minio.cloud.infn.it/ \
+    scratch https://rgw.cloud.infn.it/ \
     /scratch /s3/scratch \
     --localCache full --tryRemount --noDummyFileCheck \
     --localCacheDir "${BASE_CACHE_DIR}/scratch" \
