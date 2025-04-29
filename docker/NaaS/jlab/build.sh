2909fd417ef4e96133108e672bc4de5c78944a39
Gioacchino Vino <gioacchino.vino@infn.it>
2025-04-29 16:45:02 +0200

    edit image names

--- docker/NaaS/jlab/build.sh
+++ docker/NaaS/jlab/build.sh
@@ -1,7 +1,7 @@
 export HARBOR_CREDENTIALS='harbor-paas-credentials'
 export REGISTRY_FQDN='harbor.cloud.infn.it'
 export REPO_NAME='datacloud-templates'
-export STANDALONE_JLAB_IMAGE_NAME='jlab-k8s'
+export STANDALONE_JLAB_IMAGE_NAME='jlab-naas'
 export TAG_NAME='2.0.0-j4.1.0'
 export IMAGE_NAME="${REGISTRY_FQDN}/${REPO_NAME}/${STANDALONE_JLAB_IMAGE_NAME}:${TAG_NAME}"
 #export DOCKERFILE_PATH="docker/NaaS/jlab"
